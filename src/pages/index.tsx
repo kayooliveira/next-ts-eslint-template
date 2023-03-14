4835c44114f815c4050673bda48628963400f280
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-03-13 22:13:08 -0300

    style(Updating homepage and adding global styles to html, body tags and #__next div): :art:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello World</h1>
+      <main
+        style={{
+          backgroundColor: "#1a1b1b",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <h1
+          style={{
+            color: "cyan",
+            fontFamily: "Arial",
+            fontSize: "4rem",
+          }}
+        >
+          Next + Typescript + ESLint
+        </h1>
       </main>
     </>
   );
